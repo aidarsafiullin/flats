07cffd62452838604b10e62167ab7c0eadfc9db0
aidar07 <kzn116site@gmail.com>
2019-10-27 21:32:43 +0300

    initial add

---
+++ js/common.js
@@ -0,0 +1,49 @@
+$(function() {
+	$('.carousel').carousel({
+		interval: 2000
+	  })
+
+	$(".box_mnu .panel-heading").each(function(){
+		let ph = $(this).height() +2 ;
+		let pdt = $(this).find(".dropdown-toggle");
+		pdt.height(ph);
+	})
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+});
